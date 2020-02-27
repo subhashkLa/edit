36b476cd560ba46a92c40a11bee2d18e3ceb02dc
subhashkLa <subhashkalarathi19@gmail.com>
2020-02-28 01:00:58 +0530

    update

--- main.js
+++ main.js
@@ -6,3 +6,36 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySideNav").style.width = "0";
 }
+
+$("#carousel").owlCarousel({
+    autoplay: true,
+    lazyLoad: true,
+    loop: true,
+    margin: 20,
+    /*
+   animateOut: 'fadeOut',
+   animateIn: 'fadeIn',
+   */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 1000,
+    smartSpeed: 800,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+
+        600: {
+            items: 2
+        },
+
+        1024: {
+            items: 3
+        },
+
+        1366: {
+            items: 4
+        }
+    }
+});
