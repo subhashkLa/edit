a759207aa88dd3069b44d105c7811668132743b8
subhashkLa <sidsuvarna97@gmail.com>
2020-02-28 21:59:17 +0530

    update

--- main.js
+++ main.js
@@ -39,3 +39,20 @@ $("#carousel").owlCarousel({
         }
     }
 });
+
+$(".progress").each(function(){
+    var $bar = $(this).find(".bar");
+    var $val = $(this).find("span");
+    var percentage = parseInt( $val.text(), 10);
+
+    $({p:0}).animate({p:percentage}, {
+      duration: 3000,
+      easing: "swing",
+      step: function(p) {
+        $bar.css({
+          transform: "rotate("+ (45+(p*1.8)) +"deg)", 
+        });
+        $val.text(p|0);
+      }
+    });
+});
